8f0003b3dc57a5b237d7a7d0295451fcaa9e3a43
Julian <juliansanchezave@gmail.com>
2025-03-27 11:35:53 -0500

    APIs terminadas

--- app/api/productos_api.py
+++ app/api/productos_api.py
@@ -5,7 +5,7 @@ from app.models.models import db, Producto
 
 class ProductosApi(Resource):
     # Este decorador lanza error en la API.
-   # @cross_origin (Supuestamente no es necesario. CORS(app) se aplica globalmente a toda la aplicación)
+   # @cross_origin (Supuestamente no es necesario. CORS(app) se aplica globalmente a toda la aplicación.)
     def post (self):
        producto = Producto(nombre = request.json['nombre'], codigo = request.json['codigo'], precio = request.json['precio'])
        db.session.add(producto)
